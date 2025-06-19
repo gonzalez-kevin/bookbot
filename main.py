0702d24c049dc2dc388c56c12cff6c35ab51e2c8
gonzalez-kevin <keving12989@icloud.com>
2025-06-19 11:35:20 -0700

    Adding structured report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from stats import word_count, character_counts
+from stats import word_count, character_counts, sorted_dictionary
 
-rel_filepath = "./books/frankenstein.txt"
+rel_filepath = "books/frankenstein.txt"
 
 # storing contents of text file
 def get_book_text(filepath):
@@ -10,10 +10,18 @@ def get_book_text(filepath):
         return file_contents  
     
 word_counts = word_count(get_book_text(rel_filepath))
-character__dict = character_counts(get_book_text(rel_filepath))  
+character__dict = character_counts(get_book_text(rel_filepath))
+dictionary_list = sorted_dictionary(character__dict)
     
 def main():
-    print(f"{word_counts} words found in the document")
-    print(character__dict)
+    print("============ BOOKBOT ============\n")
+    print(f"Analyzing book found at {rel_filepath}...\n")
+    print("----------- Word Count ----------\n")
+    print(f"Found {word_counts} total words\n")
+    print("--------- Character Count -------\n")
+    for dict in dictionary_list:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}\n")
+    print("============= END ===============")
     
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,13 @@ def character_counts(book_text):
             char_dict[char] = 1
             
     return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_dictionary(dict):
+    # create list of dicts splitting the key, value pairs in the original dict
+    list_of_dicts = [{"char": key, "num": value} for key, value in dict.items()]
+    # sort list based on num value greatest to least
+    list_of_dicts.sort(reverse = True, key = sort_on)
+    return list_of_dicts
