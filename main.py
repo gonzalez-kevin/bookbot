7eb489c539c66fb022ffb27e5dac2732f9a297b3
gonzalez-kevin <keving12989@icloud.com>
2025-06-19 11:51:20 -0700

    Dynamic file reading

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from stats import word_count, character_counts, sorted_dictionary
+import sys
 
-rel_filepath = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 # storing contents of text file
 def get_book_text(filepath):
@@ -9,13 +12,13 @@ def get_book_text(filepath):
               
         return file_contents  
     
-word_counts = word_count(get_book_text(rel_filepath))
-character__dict = character_counts(get_book_text(rel_filepath))
+word_counts = word_count(get_book_text(sys.argv[1]))
+character__dict = character_counts(get_book_text(sys.argv[1]))
 dictionary_list = sorted_dictionary(character__dict)
     
 def main():
     print("============ BOOKBOT ============\n")
-    print(f"Analyzing book found at {rel_filepath}...\n")
+    print(f"Analyzing book found at {sys.argv[1]}...\n")
     print("----------- Word Count ----------\n")
     print(f"Found {word_counts} total words\n")
     print("--------- Character Count -------\n")
